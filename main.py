c0b9655a2dfad3000b4e61c838f44e364ecc0758
Your Name <you@example.com>
2022-03-20 19:29:28 +0100

    test

---
+++ main.py
@@ -0,0 +1,29 @@
+#Траектория тела в терминале 
+#x = (speed**2*math.sin(2*angle))/9,18 #на сколько метров улетел
+import math 
+import matplotlib.pyplot as plt
+import termplotlib as tpl
+import inspect
+
+x = []
+y = []
+z = []
+
+
+def landed(masse,speed,angle,maxTime):
+    for t in range(maxTime):
+        x0 = speed*math.cos(angle)*t
+        x.append(x0)
+        z0 = ((-0.5*10*x0**2)/speed**2*pow(math.cos(angle),2)) + x0*math.tan(angle)
+        z.append(z0)
+        y0 = -0,5*10*t**2 + speed*math.sin(angle)*t
+        y.append(y0)
+
+landed(10,20,math.pi/4,20)
+
+def Graph():
+    for k in z:
+        print(" "* round(k) +"#")
+        print("#")
+
+print(Graph())
